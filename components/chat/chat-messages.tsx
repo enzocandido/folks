25dbd72e40bfe50ad75cecd18d80f4c93ad3e7ae
Enzo Candido <ennzzc@gmail.com>
2024-05-12 20:27:06 -0300

    chat socket hook

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -7,6 +7,7 @@ import { Fragment } from "react";
 import { format } from "date-fns";
 import { ChatWelcome } from "./chat-welcome";
 import { ChatItem } from "./chat-item";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 const DATE_FORMAT = "d MM yyyy, HH:mm";
 
@@ -40,14 +41,23 @@ export const ChatMessages = ({
   type,
 }: ChatMessagesProps) => {
   const queryKey = `chat:${chatId}`;
+  const addKey = `chat:${chatId}:messages`;
+  const updateKey = `chat:${chatId}:messages:update`
 
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
-    useChatQuery({
+  const { 
+    data, 
+    fetchNextPage, 
+    hasNextPage, 
+    isFetchingNextPage, 
+    status 
+  } = useChatQuery({
       queryKey,
       apiUrl,
       paramKey,
       paramValue,
-    });
+  });
+
+  useChatSocket({ queryKey, addKey, updateKey })
 
   if (status === "loading") {
     return (
