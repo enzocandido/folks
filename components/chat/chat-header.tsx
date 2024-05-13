b5d393c210082409a2c023ad9eff78a7efe69006
Enzo Candido <ennzzc@gmail.com>
2024-05-13 00:00:20 -0300

    audio and video calls

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -3,6 +3,8 @@ import { MobileToggle } from "@/components/mobile-toggle";
 import { UserAvatar } from "@/components/user-avatar";
 import { SocketIndicator } from "@/components/socket-indicator";
 
+import { ChatVideoButton } from "./chat-video-button";
+
 interface ChatHeaderProps {
   serverId: string;
   name: string;
@@ -26,9 +28,10 @@ export const ChatHeader = ({
         <UserAvatar src={imageUrl} className="h-8 w-8 md:h-8 md:w-8 mr-2" />
       )}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
-        <div className="ml-auto flex items-center">
-          <SocketIndicator />
-        </div>
+      <div className="ml-auto flex items-center">
+        {type === "conversation" && <ChatVideoButton />}
+        <SocketIndicator />
+      </div>
     </div>
   );
 };
